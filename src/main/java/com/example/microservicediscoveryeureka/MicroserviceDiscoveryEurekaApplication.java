d22d69d083eab06eade4366c575889ed5619ef33
liqiang <liqiang@kkworld.com>
2018-07-19 18:53:26 +0800

    ribbon push

--- src/main/java/com/example/microservicediscoveryeureka/MicroserviceDiscoveryEurekaApplication.java
+++ src/main/java/com/example/microservicediscoveryeureka/MicroserviceDiscoveryEurekaApplication.java
@@ -2,8 +2,10 @@ package com.example.microservicediscoveryeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroserviceDiscoveryEurekaApplication {
 
 	public static void main(String[] args) {
